0b48300eecfdb3dff3e2481fdc1c607991531eb1
Roger Gabiru <roger.gabiru@gmail.com>
2025-07-28 11:49:55 -0300

    Menu responsiveness fix

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,29 @@
 const root = document.documentElement;
+const minWidth = 1300;
+const iconMenu = document.getElementById("menu-icon");
+const iconXmark = document.getElementById("xmark-icon");
 
-document.getElementById("menu-button").addEventListener("click", function () {
+function hideShowMenu() {
 	const width = root.style.getPropertyValue("--nav-width");
-
 	if (width == "0px") {
 		root.style.setProperty("--nav-width", "180px");
+		iconMenu.classList.add("hidden");
+		iconXmark.classList.remove("hidden");
 	} else {
 		root.style.setProperty("--nav-width", "0px");
+		iconMenu.classList.remove("hidden");
+		iconXmark.classList.add("hidden");
 	}
-});
+}
+
+function checkScreenWidth() {
+	const screenWidth = window.innerWidth;
+
+	if (screenWidth < minWidth) {
+		hideShowMenu();
+	}
+}
+
+document.addEventListener("DOMContentLoaded", checkScreenWidth);
+window.addEventListener("resize", checkScreenWidth);
+document.getElementById("menu-button").addEventListener("click", hideShowMenu);
